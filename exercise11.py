3c32cf4f52e6411b3ec726bbe8575d1aac6c1721
Faiza Azami <123317073+Faiza3107@users.noreply.github.com>
2023-01-31 15:05:51 +0300

    Add files via upload

---
+++ exercise11.py
@@ -0,0 +1,10 @@
+x = 'hello' #capitalize
+y = 'tom' #uppercase
+z = 'LAURA' #lowercase
+Q = 'How are you' # change o in e
+age_question = 'How old are you?' 
+a = (x.capitalize())
+b = (y.upper())
+c = (z.lower())
+question = Q.replace ('o','e')
+print(a,b,c,question,age_question)
