3360daa5e132ffee8eb938e03d6d88bae815eccb
Faiza Azami <123317073+Faiza3107@users.noreply.github.com>
2023-02-03 16:12:51 +0300

    Add files via upload

---
+++ exercisecondition3.py
@@ -0,0 +1,12 @@
+import random
+
+number1 = random.randint(1,100)
+number2 = random.randint(1,100)
+if number1 > number2 :
+    print( f'number1 :{number1} is the bigger than number2 : {number2}')
+elif number1< number2 :
+    print(f'number1 : {number1} is smaller than number2: {number2}')
+else :
+    print('{number1} is equal to {number2}')
+
+# Compare the numbers to eachother
